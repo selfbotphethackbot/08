f24a9e14465b3a4aa39d392e7d85810368a563c1
selfbotphethackbot <33895194+selfbotphethackbot@users.noreply.github.com>
2017-12-08 12:38:59 +0700

    Update ttypes.py

--- LINETCR/lib/curve/ttypes.py
+++ LINETCR/lib/curve/ttypes.py
@@ -8,7 +8,7 @@
 
 from thrift.Thrift import TType, TMessageType, TException, TApplicationException
 
-from thrift.transport import TTransport
+#from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol, TProtocol
 try:
   from thrift.protocol import fastbinary
